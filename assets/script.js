e725e321a3abd53516d4f87a39427ad997ba411a
steven rosa <scrosa13@outlook.com>
2022-05-23 14:03:32 -0400

    updated javascript and html v3

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,16 @@
 
 var quizContainer = document.getElementById("questionCard1");
 var startButton1 = document.getElementById("start1");
-var resultsContainer = document.getElementById("results");
 var startCard = document.getElementById("startCard1");
-
+var scoreCard = document.getElementById("scoreCardUser");
+var resultsContainer = document.getElementById("results");
+var leaderBoardC = document.querySelector("#leaderBoardCard")
 
 
 // var quizContainer = $('#quiz');
 
-var timer1 = 60;
-
+var timer1 = 6;
+var timerInterval;
 var timeDisplay = document.querySelector("#timer1")
 
 function displayTimer1() {
@@ -18,9 +19,10 @@ function displayTimer1() {
       timeDisplay.textContent = timer1 + " seconds remaining...";
 
       if(timer1 === 0) {
-          console.log("end time")
-          clearInterval(timerInterval);
-
+            endQuiz();
+            console.log("end time")
+            clearInterval(timerInterval);
+            
       }
   },1000);
 
@@ -38,14 +40,28 @@ var questionsEl = [
         options: ["1. A", "2. B","3. C","4. D"],
         answer: "2. B",
     },
-
-            
-    
+    {
+        questionText: "Question 3",
+        options: ["1. A", "2. B","3. C","4. D"],
+        answer: "2. B",
+    },
+    {
+        questionText: "Question 4",
+        options: ["1. A", "2. B","3. C","4. D"],
+        answer: "2. B",
+    },        
+    {
+        questionText: "Question 5",
+        options: ["1. A", "2. B","3. C","4. D"],
+        answer: "2. B",
+    },
 ];
 
 function hideCards() {
     quizContainer.setAttribute("hidden", true)
     resultsContainer.setAttribute("hidden", true)
+    scoreCard.setAttribute("hidden", true)
+    leaderBoardC.setAttribute("hidden", true)
 
 }
 
@@ -62,15 +78,16 @@ startButton1.addEventListener("click", function() {
     
     startButtonF1();
     displayTimer1();
+    currentQuestion = 0;
     renderQuestions();
 })
 
 
 
-var currentQuestion;
+
 
 function renderQuestions() {
-    currentQuestion = 0;
+    
     var question = questionsEl[currentQuestion];
     var options = question.options;
     var hQuestionEl = document.querySelector("#question1Text");
@@ -85,20 +102,146 @@ function renderQuestions() {
 
 
 
+document.querySelector("#quizOptions").addEventListener("click", verifyAnswer);
+
+var resultsText1 = document.getElementById("results");
 
 
-// function renderQuestions() {
-//     chosenQuestion = questionsEl[math.floor(math.random() * questionsEl.length)];
 
-//     for (var i = 0; i < 'variable'; i++){  }
+function optionIsCorrect(optionBtn) {
+    return optionBtn.textContent === questionsEl[currentQuestion].answer;
+}
 
-// }
+function verifyAnswer (eventObject) {
+    var optionBtn = eventObject.target;
+    resultsContainer.style.display = "block";
+if (optionIsCorrect(optionBtn)) {
+    resultsText1.textContent = "correct"
+
+} else {
+    resultsText1.textContent = "incorrect";
+
+}
 
 
-//  function createing question cards
-// var quest1 = document.createElement("div");
-// quest1.text("question 1 loading")
-// quizContainer.appendChild(quest1)
+currentQuestion ++;
+
+var score1 = document.getElementById("score1")
+
+
+if (currentQuestion < questionsEl.length) {
+    renderQuestions();
+} else {
+    endQuiz();
+    scoreCard.removeAttribute("hidden", true);
+    score1.textContent = timer1;
+}
+
+}
+
+function endQuiz() {
+    clearInterval(timerInterval);
+    quizContainer.setAttribute("hidden", true);
+
+
+}
+
+var scoreButton = document.querySelector("#scoreBtn");
+var userInput = document.querySelector("#inputFirstName")
+
+scoreButton.addEventListener("click", storeScore)
+
+// submit button not returning
+function storeScore(x) {
+    x.preventDefault();
+    if (!userInput.value) {
+        alert("Please enter your name before pressing submit!")
+        return
+    }
+
+    var leaderBoardItem = {
+        userInputN: userInput.value,
+        scoreN: timer1,
+    };
+
+    updateStoredLeaderBoard(leaderBoardItem);
+    hideCards();
+    leaderBoardC.removeAttribute("hidden", true)
+    renderLeaderBoard();
+}
+
+function updateStoredLeaderBoard(leaderBoardItem) {
+    var leaderBoardList = getLeaderBoard();
+
+    leaderBoardList.push(leaderBoardItem);
+    localStorage.setItem("leaderBoardList", JSON.stringify(leaderBoardList));
+
+}
+
+function getLeaderBoard() {
+    var storedLeaderBoard = localStorage.getItem("leaderBoardList");
+    if (storedLeaderBoard !== null) {
+        var leaderBoardList = JSON.parse(storedLeaderBoard)
+        return leaderBoardList;
+    } else {
+        leaderBoardList = [];
+    
+    }
+    return leaderBoardList;
+}
+
+function renderLeaderBoard() {
+    var sortLeaderBoardList = sortLeaderBoard();
+    var highScoreList = document.querySelector("#highScoreList")
+    highScoreList.innerHTML = " ";
+    for (let i = 0; i < sortLeaderBoardList.length; i++) {
+        var leaderBoardEntry = sortLeaderBoardList[i];
+        var newLeaderBoardItem = document.createElement("li");
+        newLeaderBoardItem.textContent = leaderBoardEntry.userInputN + " : " + leaderBoardEntry.score1;
+        highScoreList.append(newLeaderBoardItem);
+    }
+}
+
+function sortLeaderBoard() {
+    var leaderBoardList = getLeaderBoard();
+    if (!leaderBoardList) {
+        return;
+    }
+
+    leaderBoardList.sort(function (a,b) {
+        return b.score1 - a.score1;
+    });
+    return leaderBoardList
+}
+
+var clearButton = document.querySelector("#clearBTN")
+clearButton.addEventListener("click", clearHighScores);
+
+function clearHighScores () {
+    localStorage.clear();
+    renderLeaderBoard();
+}
+
+var backButton = document.querySelector("backBTN")
+backButton.addEventListener("click", returnToStart);
+
+function returnToStart() {
+    hideCards();
+    startCard.removeAttribute("hidden")
+}
+
+
+var leaderBoardLink = document.querySelector("LeaderBoardLink")
+leaderBoardLink.addEventListener("click", showLeaderBoard);
+
+function showLeaderBoard() {
+    hideCards();
+    leaderBoardC.removeAttribute("hidden")
+    clearInterval(timer1)
+    renderLeaderBoard();
+}
+
+
 
 // function saveLastScore() {
 //     var userScore = {
